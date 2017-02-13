d2e4f76137a4703e8b7c624e0e3c5bd89dcbc94f
sli41 <sli41@ncsu.edu>
2017-02-12 19:54:48 -0500

    update imported packages in class FrequencySummarizer

---
+++ NLP/FrequencySummarizer.py
@@ -0,0 +1,11 @@
+# coding: uft-8
+
+from nltk.tokenize import sent_tokenize, word_tokenize # tokenize text into sentences and words
+from nltk.corpus import stopwords # find and filter very common words
+
+from collections import defaultdict # dictionary with default value
+
+from string import punctuation # punctuation symbols
+
+from heapq import nlargest # return n largest elements in given list
+
